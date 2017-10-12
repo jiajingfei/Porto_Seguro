d05b3ad65226814a4d490196d4a60df186bef054
catherinexin88 <catherine.xcheng@gmail.com>
2017-10-12 00:19:48 -0400

    :feature extractor refactoring

--- repo_refactoring/code/feature_extraction/feature_extraction.py
+++ repo_refactoring/code/feature_extraction/feature_extraction.py
@@ -4,31 +4,38 @@ sys.path.append(os.path.join(os.path.dirname(__file__), '../'))
 import pandas as pd
 from configs.base_config import *
 from abc import ABCMeta, abstractmethod
-from utils import outputfile_already_exists
+from utils import save_df_to_file
 
-class Proto():
+class FeatureExtractor():
     def __init__(
             self,
-            id_col='id',
-            cols=None # Cols to use for feature extraction, default is all but id_col
+            df_or_csv,
+            id_col = 'id',
+            cols = None # Cols to use for feature extraction
     ):
+        if isinstance(df_or_csv, str):
+            self._df = pd.read_csv(df_or_csv)
+        else:
+            self._df = df_or_csv 
         self._id_col = id_col
-        return None
+        self._cols = cols
 
-    @abstractmethod
-    def apply(self, df):
-        # Return some feature dataframe
-        return None
+    def PeekDF(self):
+        return self._df
 
-    def apply_on_file(
+    def DoNothing(self):
+        print "Do Nothing."
+
+    # Other feature operations to manipulate self._df
+
+    def SaveToFile(
             self,
-            input_file,
             output_file,
             overwrite=False
     ):
-        df = pd.read_csv(input_file)
-        # CR jjia: could check the last modified time to determine whether to rerun this
-        if not outputfile_already_exists(output_file) or overwrite:
-            self.apply(df).to_csv(output_file)
-        else:
-            print "WARNING: output file for {} already exists".format(self.__class__.__name__)
+        save_df_to_file(self._df, output_file, overwrite)
+            
+if __name__ == '__main__':
+    extractor = FeatureExtractor('../../data/train.csv')
+    extractor.DoNothing()
+    extractor.SaveToFile('../../data/extractor_test.csv')
--- repo_refactoring/code/utils.py
+++ repo_refactoring/code/utils.py
@@ -1,12 +1,16 @@
 import os
 
-def outputfile_already_exists(filename, create_path_if_not_exist=True):
-    if os.path.isfile(filename):
-        return True
-    elif not create_path_if_not_exist:
-        return False
-    else:
-        path, filename = os.path.split(filename)
-        if not os.path.isdir(path):
-            os.system('mkdir {}'.format(path))
-        return False
+def save_df_to_file(df, output_file, overwrite):
+    if os.path.isfile(output_file):
+        print "Output file exists."
+        # CR jjia: could check the last modified time to determine whether to rerun this
+        if not overwrite:
+            print "Not overwrite."
+            # raise
+            return
+        else:
+            print "Will overwirte."
+    path, filename = os.path.split(output_file)
+    if not os.path.isdir(path):
+        os.system('mkdir -p {}'.format(path))
+    df.to_csv(output_file, index = False)
