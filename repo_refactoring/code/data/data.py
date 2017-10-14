14006e1af8704228d288754f2f59025b93a6ac09
jiajingfei <jiajingfei@gmail.com>
2017-10-13 23:07:51 -0400

    added split

--- repo_refactoring/code/data/data.py
+++ repo_refactoring/code/data/data.py
@@ -3,23 +3,32 @@ import sys
 import pandas as pd
 import datetime as dt
 import argparse
+import numpy as np
 from sklearn.utils import shuffle
 from sklearn.model_selection import StratifiedShuffleSplit
 sys.path.append(os.path.join(os.path.dirname(__file__), '../'))
-from configs.base_config import *
+from utils import gini_normalized
+from configs.base_config import (
+    id_col,
+    label_col,
+    base_data_dir,
+    train_file,
+    test_file,
+    test_target_file,
+    raw_data_dir,
+    sanity_data_dir
+)
 
 class Training_data(object):
-    def __init__(self, directory, id_col, label_col):
-        self._id_col = id_col
-        self._label_col = label_col
+    def __init__(self, directory):
         self._train = pd.read_csv(os.path.join(directory, train_file))
         return None
 
     def __output_generated_data(self, train, test, output_directory):
         if not os.path.exists(output_directory):
             os.system('mkdir -p {}'.format(output_directory))
-        test_target = test[[self._label_col]]
-        del test[self._label_col]
+        test_target = test[[id_col, label_col]]
+        del test[label_col]
         train.to_csv(os.path.join(output_directory, train_file), index=False)
         test.to_csv(os.path.join(output_directory, test_file), index=False)
         test_target.to_csv(os.path.join(output_directory, test_target_file), index=False)
@@ -41,7 +50,7 @@ class Training_data(object):
         df = shuffle(self._train)
         train = df[:nrows].copy()
         test = df[nrows:2*nrows].copy()
-        test_target = test[[self._label_col]]
+        test_target = test[[label_col]]
         self.__output_generated_data(train, test, output_directory)
 
     # Default output_directory is base_data_dir + today's date
@@ -68,8 +77,34 @@ class Training_data(object):
             results.append((df.iloc[train_ind], df.iloc[valid_idn]))
         return results
 
+
+class Prediction(object):
+    def __init__(self, df):
+        self._df = df.sort_values(id_col).reset_index(drop=True)
+
+    def eval(self, data_dir):
+        filename = os.path.join(data_dir, test_target_file)
+        if not os.path.isfile(filename):
+            raise Exception('{} does not exist, is this raw file'.format(filename))
+        test_target = pd.read_csv(filename)
+        test_target = test_target.sort_values(id_col).reset_index(drop=True)
+        if not test_target[id_col].equals(self._df[id_col]):
+            raise Exception('id cols are not consistent')
+        else:
+            return gini_normalized(test_target[label_col], self._df[label_col])
+
+def test_predcition():
+    if not os.path.exists(sanity_data_dir):
+        training_data = Training_data(raw_data_dir)
+        training_data.output_small_data_for_sanity_check(sanity_data_dir)
+    df = pd.read_csv(os.path.join(sanity_data_dir, test_target_file))
+    print df.head()
+    prediction = Prediction(df)
+    normalized_gini = prediction.eval(sanity_data_dir)
+    assert (np.isclose(normalized_gini, 1))
+
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser()
+    parser = argparse.ArgumentParser(description='split training data into training and validation dataset')
     parser.add_argument(
         '--mode',
         '-m',
@@ -79,7 +114,7 @@ if __name__ == '__main__':
         help=' mode (sanity|split)'
     )
     args = parser.parse_args()
-    training_data = Training_data(raw_data_dir, id_col, label_col)
+    training_data = Training_data(raw_data_dir)
     if args.mode == 'sanity':
         training_data.output_small_data_for_sanity_check(output_directory=sanity_data_dir)
     elif args.mode == 'split':
--- repo_refactoring/code/feature_extraction/raw_features.py
+++
@@ -1,9 +0,0 @@
-import os
-import sys
-sys.path.append(os.path.join(os.path.dirname(__file__), './'))
-from feature_extraction import Proto as P
-
-# Example of raw features extraction
-class Raw_features(P):
-    def apply(self, df):
-        return df
