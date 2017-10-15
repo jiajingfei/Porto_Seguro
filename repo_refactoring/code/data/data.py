d1dd329228a5effdbbae8f1d263b3ea51c818944
jiajingfei <jiajingfei@gmail.com>
2017-10-15 15:11:48 -0400

    update data.py

--- repo_refactoring/code/data/data.py
+++ repo_refactoring/code/data/data.py
@@ -6,10 +6,12 @@ import argparse
 import getpass
 import numpy as np
 from sklearn.utils import shuffle
-from sklearn.model_selection import StratifiedShuffleSplit
+from sklearn.model_selection import StratifiedShuffleSplit, StratifiedKFold
 sys.path.append(os.path.join(os.path.dirname(__file__), '../'))
-from utils import gini_normalized
-from configs.base_config import (
+from utils import gini_normalized, save_df_to_file
+import configs.base_config as config
+'''
+from configs.base_config import config
     id_col,
     label_col,
     get_data_dir,
@@ -21,26 +23,27 @@ from configs.base_config import (
     data_sanity_dir,
     get_pred_dir,
     pred_filename,
-    pred_log_file,
+    pred_log_file
 )
+'''
 
 class Training_data(object):
     def __init__(self, dir_or_df):
-        if os.path.isdir(get_data_dir(dir_or_df)):
-            self._train = pd.read_csv(data_train_file(dir_or_df))
+        if os.path.isdir(config.get_data_dir(dir_or_df)):
+            self._train = pd.read_csv(config.data_train_file(dir_or_df))
         else:
             self._train = dir_or_df.copy()
         return None
 
     def __output_generated_data(self, train, test, output_directory):
-        if not os.path.exists(get_data_dir(output_directory)):
-            os.system('mkdir -p {}'.format(get_data_dir(output_directory)))
-        test_target = test[[id_col, label_col]]
-        del test[label_col]
-        train.to_csv(data_train_file(output_directory), index=False)
-        test.to_csv(data_test_file(output_directory), index=False)
-        test_target.to_csv(data_test_target_file(output_directory), index=False)
-        with open(data_readme(output_directory), 'w') as f:
+        if not os.path.exists(config.get_data_dir(output_directory)):
+            os.system('mkdir -p {}'.format(config.get_data_dir(output_directory)))
+        test_target = test[[config.id_col, config.label_col]]
+        del test[config.label_col]
+        train.to_csv(config.data_train_file(output_directory), index=False)
+        test.to_csv(config.data_test_file(output_directory), index=False)
+        test_target.to_csv(config.data_test_target_file(output_directory), index=False)
+        with open(config.data_readme(output_directory), 'w') as f:
             readme_lines = [
                 'this directory is for generated training data and validation data\n',
                 'it should contain a train file with labels and a test file without labels\n',
@@ -49,7 +52,7 @@ class Training_data(object):
             f.writelines(readme_lines)
 
     def output_small_data_for_sanity_check(self, nrows=500):
-        data_dir = get_data_dir(data_sanity_dir)
+        data_dir = config.get_data_dir(config.data_sanity_dir)
         if not os.path.exists(data_dir):
             os.system('mkdir -p {}'.format(data_dir))
         if os.path.exists(data_dir):
@@ -59,15 +62,15 @@ class Training_data(object):
         df = shuffle(self._train)
         train = df[:nrows].copy()
         test = df[nrows:2*nrows].copy()
-        test_target = test[[label_col]]
-        self.__output_generated_data(train, test, data_sanity_dir)
+        test_target = test[[config.label_col]]
+        self.__output_generated_data(train, test, config.data_sanity_dir)
 
     # Default output_directory is base_data_dir + today's date
     def output_split(self, validation_proportion=0.3, output_directory=None):
         if output_directory is None:
             output_directory = '{}'.format(dt.date.today ())
 
-        data_dir = get_data_dir(output_directory)
+        data_dir = config.get_data_dir(output_directory)
         if os.path.exists(data_dir):
             data_dir = os.path.abspath(data_dir)
             raise Exception('{} already exists'.format(data_dir))
@@ -82,14 +85,14 @@ class Training_data(object):
 
     def kfold(self, n_splits, random_state):
         df = self._train.copy()
-        if n_splits = None:
+        if n_splits is None:
             return [(df, None)]
         else:
             assert (n_splits > 1)
             results = []
             sss = StratifiedKFold(n_splits, random_state=random_state)
             for (train_ind, valid_ind) in sss.split(df, df.target):
-                results.append((df.iloc[train_ind], df.iloc[valid_idn]))
+                results.append((df.iloc[train_ind], df.iloc[valid_ind]))
             return results
 
 
@@ -98,30 +101,31 @@ class Prediction(object):
     def __init__(self, df, data_dir, identifier):
         self._dir = data_dir
         self._identifier = identifier
-        self._df = df.sort_values(id_col).reset_index(drop=True)
+        self._df = df.sort_values(config.id_col).reset_index(drop=True)
 
     def eval(self):
-        filename = data_test_target_file(self._dir)
+        filename = config.data_test_target_file(self._dir)
         if not os.path.isfile(filename):
             return None
 
         test_target = pd.read_csv(filename)
-        test_target = test_target.sort_values(id_col).reset_index(drop=True)
-        if not test_target[id_col].equals(self._df[id_col]):
+        test_target = test_target.sort_values(config.id_col).reset_index(drop=True)
+        if not test_target[config.id_col].equals(self._df[config.id_col]):
             raise Exception('id cols are not consistent')
         else:
-            return gini_normalized(test_target[label_col], self._df[label_col])
+            return gini_normalized(test_target[config.label_col], self._df[config.label_col])
 
     def eval_output_and_register(self, filename):
         gini = self.eval()
-        pred_dir = get_pred_dir(self._dir)
-        pred_log_file = pred_log_file(self._dir)
-        pred_filename = pred_filename(self._dir, filename, self._identifier)
-        if not os.path.exists(pred_dir):
-            f = open(pred_log_file, 'ab')
-            f.write('data_dir,gini,user,time,identifier')
+        pred_dir = config.get_pred_dir(self._dir)
+        log_file = config.pred_log_file(self._dir)
+        filename = config.pred_filename(self._dir, filename, self._identifier)
+        if not os.path.exists(log_file):
+            os.system('mkdir -p {}'.format(pred_dir))
+            f = open(log_file, 'w')
+            f.write('data_dir,gini,user,time,identifier\n')
         else:
-            f = open(pred_log_file, 'ab')
+            f = open(log_file, 'ab')
         new_line = '{},{},{},{},{}'.format(
             self._dir,
             gini,
@@ -129,13 +133,15 @@ class Prediction(object):
             dt.datetime.now(),
             self._identifier
         )
+        f.write(new_line)
+        save_df_to_file(self._df, filename, overwrite=False)
 
 def test_predcition():
-    if not os.path.exists(get_data_dir(data_sanity_dir)):
-        training_data = Training_data(data_raw_dir)
-        training_data.output_small_data_for_sanity_check(data_sanity_dir)
-    df = pd.read_csv(data_test_target_file(data_sanity_dir))
-    prediction = Prediction(df, data_sanity_dir, identifier="sanity_check")
+    if not os.path.exists(config.get_data_dir(config.data_sanity_dir)):
+        training_data = Training_data(config.data_raw_dir)
+        training_data.output_small_data_for_sanity_check(config.data_sanity_dir)
+    df = pd.read_csv(config.data_test_target_file(config.data_sanity_dir))
+    prediction = Prediction(df, config.data_sanity_dir, identifier="sanity_check")
     normalized_gini = prediction.eval()
     assert (np.isclose(normalized_gini, 1))
 
@@ -150,7 +156,7 @@ if __name__ == '__main__':
         help=' mode (sanity|split)'
     )
     args = parser.parse_args()
-    training_data = Training_data(data_raw_dir)
+    training_data = Training_data(config.data_raw_dir)
     if args.mode == 'sanity':
         training_data.output_small_data_for_sanity_check()
     elif args.mode == 'split':
