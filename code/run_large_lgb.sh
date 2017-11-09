cfef979b27e1f945c4ce85d8f2cf4943a3d261c2
jiajingfei <jiajingfei@gmail.com>
2017-11-08 23:57:49 -0500

    fixed typo

--- code/run_large_lgb.sh
+++ code/run_large_lgb.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-python new_lgb_experiments.py -f raw_data/set0,raw_data/set1,raw_data/set2,raw_data/set3,raw_data/set4 -a raw_more -n 100 -s raw
+python new_lgb_experiments.py -f raw_data/set0,raw_data/set1,raw_data/set2,raw_data/set3,raw_data/set4 -a raw -n 100 -s raw
 python new_lgb_experiments.py -f raw_data/set0,raw_data/set1,raw_data/set2,raw_data/set3,raw_data/set4 -a raw_more -n 100 -s raw_more
 python new_lgb_experiments.py -f raw_data/set0,raw_data/set1,raw_data/set2,raw_data/set3,raw_data/set4 -a one_hot -n 100 -s one_hot
 python new_lgb_experiments.py -f raw_data/set0,raw_data/set1,raw_data/set2,raw_data/set3,raw_data/set4 -a reorder -n 100 -s reorder
